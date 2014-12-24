709595364f24c036401955ee84ccfc59b1584762
guspowell <augustuspowell@me.com>
2014-12-24 15:49:16 +0000

    added list of items in hash

---
+++ lib/menu.rb
@@ -0,0 +1,10 @@
+class Menu
+
+	menu = {
+		pepperoni: 12
+		hawaii: 11
+		bbq_chicken: 11
+		margaritta: 9
+	}
+
+end
---
+++ spec/menu_spec.rb
@@ -0,0 +1,9 @@
+require menu
+
+describe Menu do
+
+	it 'should have a list of all items and prices' do
+
+	end
+
+end
